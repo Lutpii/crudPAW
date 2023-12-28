8406b943ed8b2cfc3f0764182db036fda81ce778
Adi Luthfi Nur Roki <adiluthfi03@gmail.com>
2023-12-28 20:04:52 +0700

    letsgo

---
+++ src/main/java/projectPAW/projectPAW/ProjectPawApplication.java
@@ -0,0 +1,13 @@
+package projectPAW.projectPAW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projectPAW/projectPAW/ProjectPawApplicationTests.java
@@ -0,0 +1,13 @@
+package projectPAW.projectPAW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
